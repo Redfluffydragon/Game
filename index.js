53787e1bcecbfd3792976693d7d0bebc6036e879
Redfluffydragon <kderu1420@gmail.com>
2020-03-15 20:23:18 -0600

    back button

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let startbtn = document.getElementById("startbtn");
 let ssbtnsdiv = document.getElementById("ssbtnsdiv");
 startbtn.addEventListener("click", start, false);
 function start(){
-  document.body.style.background = "#ffffff";
+  document.body.style.background = "#ff00c8";
   youFool.style.display = "block";
   ssbtnsdiv.style.display = "none";
 };
